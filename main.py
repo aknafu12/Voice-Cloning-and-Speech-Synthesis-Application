cab024b248e755e9dcc8cff8d659a8502258ea03
aknafu12 <kgebre2021@gmail.com>
2024-03-20 10:01:32 +0300

    initial design

---
+++ main.py
@@ -0,0 +1,4 @@
+import voice_cloning_app
+
+if __name__ == "__main__":
+    
---
+++ voice_cloning_app.py
@@ -0,0 +1,12 @@
+# Define a class for the voice cloning application
+class VoiceCloningApp:  
+    def __init__(self):  
+        pass
+
+    def load_audio_file(self):  # Method to load audio file
+        pass
+
+    def clone_voice(self):  # Method to clone voice
+        pass
+
+       
